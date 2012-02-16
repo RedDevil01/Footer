f9be1b29bdc0b3b2b7ac71cc5fa97082c3b7b102
Harish Sp <harish00@gmail.com>
2012-02-16 10:35:26 +0530

    Footer First Commit

---
+++ src/script/CommonFooter.js
@@ -0,0 +1,36 @@
+﻿(function () {
+
+    var FooterControl;
+
+    window.FooterControl = FooterControl = (function () {
+
+        function FooterControl() { }
+
+        FooterControl.prototype._create = function () {
+            var FooterInner;
+            FooterInner = ($('<div/>')).addClass(this.css.FooterInner).append(($('<a/>')).attr({
+                href: this.options.Link
+            }).text(this.options.Text).addClass(this.css.Link));
+            return ($(this.element)).attr({
+                "align": "center"
+            }).append(FooterInner).addClass(this.css.WrapperFooter);
+        };
+
+        FooterControl.prototype.options = {
+            Name: "Default",
+            Link: "javascript:void(0);",
+            Text: "Copyright 2010 Verizon Communications"
+        };
+
+        FooterControl.prototype.css = {
+            WrapperFooter: "ui-widget-footer",
+            FooterInner: "ui-widget-footer-inner"
+        };
+
+        return FooterControl;
+
+    })();
+
+    $.widget("vdms.CommonFooter", new FooterControl);
+
+}).call(this);
